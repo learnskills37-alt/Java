c208977203170b6f277e1f5b7f9b53b187339cee
learnskills37-alt <learnskills37@gmail.com>
2026-07-08 09:50:17 +0530

    Add files via upload

---
+++ Employeem.java
@@ -0,0 +1,19 @@
+class Employee
+{
+  String name="ABC",id="516",or="Infy";
+void display()
+{
+  System.out.println("Name"+name+"ID"+id+"organization"+or);
+}
+}
+class Employeem
+{
+   public static void main(String[] args)
+{
+   Employee o1=new Employee();
+o1.display();
+}
+}
+
+
+    
