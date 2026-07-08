d0d0a79d43883af42d7e6b9cb0548fc134f61806
learnskills37-alt <learnskills37@gmail.com>
2026-07-08 10:11:14 +0530

    Add files via upload

---
+++ Studm.java
@@ -0,0 +1,18 @@
+class Stud
+{
+  String n="ABC",rn="123",cn="GNITS";
+void display()
+{
+  System.out.println("Name is"+n);
+  System.out.println("Rollno is"+rn);
+  System.out.println("College name is"+cn);
+}
+}
+class Studm
+{
+   public static void main(String[] args)
+{
+  Stud o1=new Stud();
+o1.display();
+}
+}
